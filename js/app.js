10161f7bf7ff8e3595dc45dd3b94fa5d970eec86
jibe-b <user701@orange.fr>
2019-06-29 00:38:45 +0200

    Ajoute le js pour une bannière 'Ajouter à l'écran d'accueil

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  showAddToHomeScreen();
+
+});
+
+
+
+function showAddToHomeScreen() {
+
+  var a2hsBtn = document.querySelector(".ad2hs-prompt");
+
+  a2hsBtn.style.display = "block";
+
+  a2hsBtn.addEventListener("click", addToHomeScreen);
+
+}
+
